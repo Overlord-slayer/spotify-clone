beb4c06d46c5ca5463969f9f4bac1fc1ca65f6ad
Samuel Argueta <jarguetahernandez02@gmail.com>
2023-11-28 15:48:57 -0600

    Continuando el tutorial

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,6 +6,8 @@ import { useMemo }  from "react"
 import { HiHome } from "react-icons/hi"
 import { BiSearch } from "react-icons/bi"
 import Box from "./Box"
+import SideBarItem from "./SideBarItem"
+import Library from "./Library"
 
 interface SideBarProps{
   children: React.ReactNode
@@ -42,9 +44,30 @@ const SideBar: React.FC<SideBarProps> = ({
         p-2
       ">
         <Box>
-          SideBar navigation
+          <div 
+            className="
+              flex
+              flex-col
+              gap-y-4
+              px-5
+              py-4
+            "
+          >
+            {routes.map((item) => (
+              <SideBarItem
+                key={item.label}
+                {...item}
+              />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          <Library/>
         </Box>
       </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+      </main>
     </div>
   )
 }
